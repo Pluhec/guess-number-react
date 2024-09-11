8d5519a6d316f9fd94b2349833dabdc42b359256
Pluhec <jakplu@seznam.cz>
2024-09-11 19:47:19 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useState, useEffect } from 'react'
 
-function App() {
+
+const App = () => {
+  const [userNumber, setUserNumber] = useState()
+  const [randomNumber, setRandomNumber] = useState("")
+  const [answer, setAnswer] = useState("")
+
+  console.log(userNumber)
+  console.log(randomNumber)
+
+  const generateNumber = () => {
+    setRandomNumber(Math.floor(Math.random() * 10)) 
+    setAnswer("")
+  }
+
+  useEffect( () => {
+    generateNumber()
+  },[])
+
+  const guessing = (e) =>{
+    e.preventDefault()
+    if(userNumber == randomNumber){
+      setAnswer("vyhral jsi")
+    }else{
+      setAnswer("zkus jine cislo")
+    }
+  }
+
+  const showAnswer = () => {
+    let answerNumber = document.getElementById('answerNumber')
+    if(answerNumber.style.display == "block"){
+      answerNumber.style.display = "none"
+    }else{
+      answerNumber.style.display = "block"
+    }
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="game-container">
+      <h1 className="main-text" id="text">Guess the number</h1>
+      <p className="answer-number" id="answerNumber">{randomNumber}</p>
+      <button className="btn" onClick={showAnswer}>Show the number</button>
+      <form className="form" onSubmit={guessing}>
+        <input className="input" type="number" onChange={ (e) => {setUserNumber(e.target.value)} } />
+        <input className="btn" value="Tipnout" type="submit"/>
+      </form>
+      <button className="btn" onClick={generateNumber}>new number</button>
+      <h1>{answer}</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
